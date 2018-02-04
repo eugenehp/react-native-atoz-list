4f71ff887b927022b59b1fb521164a35602dd835
Eugene Hauptmann <eugene.hp2012@gmail.com>
2018-02-03 23:28:45 -0800

    Update AlphabetPicker.js

--- AlphabetPicker.js
+++ AlphabetPicker.js
@@ -85,7 +85,7 @@ export default class AlphabetPicker extends Component {
                     letter={letter}
                     textPickerStyle={textPickerStyle}
                 />
-            )
+            ))
         );
 
         return (
